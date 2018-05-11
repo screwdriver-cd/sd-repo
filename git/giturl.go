3196dfbef3f5e53b8c38d2aca3f36c52591ff411
Zhongtang Tian <tianzhongtang@gmail.com>
2018-05-11 11:18:45 -0700

    feat: added a simple model that is able to parse git url with optiona… (#1)
    * feat: added a simple model that is able to parse git url with optional branch and path
    
    * addressed PR comments

---
+++ git/giturl.go
@@ -0,0 +1,50 @@
+package git
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+)
+
+type GitUrl struct {
+	Host   string
+	Org    string
+	Repo   string
+	Path   string
+	Branch string
+}
+
+func (git *GitUrl) GetCloneInfo() (url, branch string) {
+	return fmt.Sprintf("git@%+s:%+s/%+s.git", git.Host, git.Org, git.Repo), git.Branch
+}
+
+func New(gitUrlStr string) (*GitUrl, error) {
+	// This would match something like git@github.com:org/repo.git/path#branch
+	// path and branch are optional. If not given, default values are "" and "master"
+	gitUrlRegex, _ := regexp.Compile("^git@([^/:#]+):([^/:#]+)/+([^/:#]+)\\.git(/[^#]*)?(#.+)?")
+	parseResult := gitUrlRegex.FindStringSubmatch(gitUrlStr)
+
+	if parseResult == nil {
+		return nil, fmt.Errorf("Error: not a valid git url %+s", gitUrlStr)
+	}
+
+	gitUrl := GitUrl{
+		Host:   parseResult[1],
+		Org:    parseResult[2],
+		Repo:   parseResult[3],
+		Path:   parseResult[4],
+		Branch: parseResult[5],
+	}
+
+	if gitUrl.Branch == "" {
+		gitUrl.Branch = "master"
+	} else {
+		gitUrl.Branch = strings.TrimPrefix(gitUrl.Branch, "#")
+	}
+
+	if gitUrl.Path != "" {
+		gitUrl.Path = strings.TrimLeft(gitUrl.Path, "/")
+	}
+
+	return &gitUrl, nil
+}
